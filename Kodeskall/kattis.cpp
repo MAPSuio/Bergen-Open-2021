3601b2d05d2df6b46afc8d63590299595bf82b31
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-10-28 18:00:46 +0200

    Create kattis.cpp

---
+++ Kodeskall/kattis.cpp
@@ -0,0 +1,15 @@
+// Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output.
+
+// Her kan man enkelt lese inn ved å bruke cin >>
+// For output skal man ikke returne noe. Vi ønsker bare å printe, vanlig cout <<
+
+// Her følger et kodeskall du kan bruke:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Lykke til!
+  
+   return 0;
+}
