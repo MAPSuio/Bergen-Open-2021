a29fd0653224494a535ce16488c361532d52309e
Sarek Høverstad Skotåm <44001885+sarsko@users.noreply.github.com>
2021-10-28 20:05:54 +0200

    Create kattis.kt

---
+++ Kodeskall/kattis.kt
@@ -0,0 +1,15 @@
+/*
+  Under er et eksempel på en kodeflyt i Kotlin.
+  Denne leser inn `n`, og så leser den inn n linjer, der hver linje
+  er en serie med integere som er separert av mellomrom.
+*/
+
+fun main() {
+    val n = readLine()!!.toInt()
+    var res = 0
+    for (i in 0..n-1) {
+        var line = readLine()!!.split(" ").toMutableList().map{ it.toInt() }
+        // Gjør ting
+    }
+    println(res)
+}
