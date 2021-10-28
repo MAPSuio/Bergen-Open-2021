62c2aad1cd7d179acb5932ae43e2ae7cc3e1f252
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-10-28 19:52:52 +0200

    Endret kodeskall til kodeeksempel i Python

--- Kodeskall/kattis.py
+++ Kodeskall/kattis.py
@@ -1,11 +1,30 @@
-# Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output.
-# Man kan enkelt lese inn ved å bruke input(), og int(input()) der det passer seg.
-# For output skal man ikke returne noe. Vi ønsker bare å printe, vanlig print()
+"""
+Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output.
+Man kan lese inn med standard input(), og skrive med standard print()
 
-# Her følger et kodeskall du kan bruke:
+Under følger et eksempel på en løsning til følgende problem:
+"Gi meg summen av n heltall"
+
+Input:
+5
+60
+34
+2
+13
+20
+
+Output:
+134
+"""
 
 def main():
-    pass
+    n = int(input()) # Det første tallet, som markerer hvor mange tall som følger
+    s = 0
+    
+    for _ in range(n): # Gjør noe med de neste n tallene
+        sum += int(input())
+
+    print(s)
 
 if __name__ == "__main__":
    main()
