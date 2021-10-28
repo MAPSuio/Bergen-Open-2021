a9d58c5f79198f8bfa72d22b90dcdc05e859e70a
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-10-28 18:01:06 +0200

    Update kattis.java

--- Kodeskall/kattis.java
+++ Kodeskall/kattis.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 class Kattis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        // Lykke til!
         sc.close();
     }
 }
