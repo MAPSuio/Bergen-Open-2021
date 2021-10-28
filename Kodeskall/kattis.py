bdd868c73d4e68db7c36db691c8e0a78a6bb3d23
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-10-28 17:53:26 +0200

    Update kattis.py

--- Kodeskall/kattis.py
+++ Kodeskall/kattis.py
@@ -1,4 +1,4 @@
-# Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output
+# Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output.
 # Man kan enkelt lese inn ved å bruke input(), og int(input()) der det passer seg.
 # For output skal man ikke returne noe. Vi ønsker bare å printe, vanlig print()
 
