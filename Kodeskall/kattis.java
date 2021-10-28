80a9168df6096ba1beeec571f462050d68283016
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-10-28 17:55:16 +0200

    Kodeskall for Java

---
+++ Kodeskall/kattis.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output.
+
+// Man kan løse dette ved å importere Scanner og så ta System.in som parameter.
+// Deretter kan man blant annet lese Strings med scanner.readNextLine() og ints med scanner.nextInt()
+// Du kan finne alt på https://docs.oracle.com/javase/7/docs/api/java/util/Scanner.html
+
+// For output skal man ikke returne noe. Vi ønsker bare å printe, vanlig System.out.println()
+
+// Her følger et kodeskall du kan bruke:
+
+class Kattis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+}
