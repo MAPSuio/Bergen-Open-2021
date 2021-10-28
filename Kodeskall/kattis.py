13cc58aed087033575d33ffd518dc2ab6bc88684
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-10-28 17:49:37 +0200

    Kodeskall for Python

---
+++ Kodeskall/kattis.py
@@ -0,0 +1,11 @@
+# Vi ønsker å lese noe input, anvende det på en algoritme, og produsere noe output
+# Man kan enkelt lese inn ved å bruke input(), og int(input()) der det passer seg.
+# For output skal man ikke returne noe. Vi ønsker bare å printe, vanlig print()
+
+# Her følger et kodeskall du kan bruke:
+
+def main():
+    pass
+
+if __name__ == "__main__":
+   main()
